33851eb620cd64c667ee5cd346bafabd54ab9ee0
thor-odinson-17 <63148306+thor-odinson-17@users.noreply.github.com>
2020-05-05 23:05:51 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Yay your userbot is officially working. You should owe @pureindialover")
+print("Yay! your userbot is officially working and you are a part of IndianBot. You should owe @pureindialover and @tansique_17")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
