ef78da0044c2f3b11a24b1f0f6babe4fded2ec39
Restyled.io <commits@restyled.io>
2020-07-03 14:40:47 +0000

    Restyled by autopep8

--- userbot/plugins/pureindialover.py
+++ userbot/plugins/pureindialover.py
@@ -16,22 +16,21 @@ async def _(event):
     if event.fwd_from:
         return
     animation_interval = 1.5
-    animation_ttl = range(0,36)
+    animation_ttl = range(0, 36)
     #input_str = event.pattern_match.group(1)
    # if input_str == "pureindialover":
     await event.edit("@pureindialover")
     animation_chars = [
-            "@pureindialover mera Loda",
-            "@pureindialover Randi ka baccha",
-            "@pureindialover Apni Ma chod ke apne bhai ko jan dene wala",
-            "@pureindialover owner of @Randi_hu_mai Army ",
-            "Loda Aur Lund Nahi Hai mere Pas",
-            "Mere Baap Ka loda chahiye mujhko",
-            "@pureindialover"
-         ]
-            
+        "@pureindialover mera Loda",
+        "@pureindialover Randi ka baccha",
+        "@pureindialover Apni Ma chod ke apne bhai ko jan dene wala",
+        "@pureindialover owner of @Randi_hu_mai Army ",
+        "Loda Aur Lund Nahi Hai mere Pas",
+        "Mere Baap Ka loda chahiye mujhko",
+        "@pureindialover"
+    ]
 
     for i in animation_ttl:
-        	
+
         await asyncio.sleep(animation_interval)
         await event.edit(animation_chars[i % 18])
